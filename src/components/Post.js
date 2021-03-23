14701f244eea1fc78cb99bb0d92ef172d536e981
afraefatuaki <afrae@elfatuaki.com>
2021-03-23 15:37:28 +0100

    created-post

---
+++ src/components/Post.js
@@ -0,0 +1,48 @@
+const Post = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <header>
+                <h1>Harvard/<br />Art Museum</h1>
+            </header>
+            <main>
+                <section>
+                    <img src="" />
+                    <div>
+                        <h1></h1>
+                        <p></p>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <h3></h3>
+                        <p></p>
+                        <br />
+
+                        <h3></h3>
+                        <p></p>
+                        <br />
+
+                        <h3></h3>
+                        <p></p>
+                        <br />
+
+                        <h3></h3>
+                        <p></p>
+                    </div>
+
+                    <div>
+                        <h3></h3>
+                        <p></p>
+                        <br />
+
+                        <h3></h3>
+                        <p></p>
+                    </div>
+                </section>
+            </main>
+        </div>
+    );
+}
+
+export default Post;
