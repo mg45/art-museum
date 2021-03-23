9f079d78406d2cfcc03100adbc16c19426beeeb3
mg45 <m.golabski@gmx.de>
2021-03-23 17:13:13 +0100

    new components with props

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import Post from '../Post/Post';
 
 class PostList extends Component {
     state = {
@@ -8,7 +9,6 @@ class PostList extends Component {
 
     componentDidMount () {
         axios.get('https://api.harvardartmuseums.org/object?classification=Sculpture&century=14th%20century&apikey=1da9b44f-392a-4d5f-8782-ff00202ed72a&size=100')
-        
         .then(response => {
             this.setState({posts: response.data.records});
             console.log(response.data.records);
@@ -17,8 +17,14 @@ class PostList extends Component {
 
     render() {
         const posts = this.state.posts.map(post => {
-            console.log(post);
-            return [post.classification, post.id]
+            //console.log(post);
+            //return [post.classification, post.id]
+            return <Post 
+                id={post.id}
+                title={post.classification}
+                url={post.primaryimageurl}
+                contact={post.contact}
+                clicked={() => this.postSelectedHandler(post.id)} />;
         })
 
         return (
